f2a1a4937daaa6ffa31e4a0d825d8925cb042ade
Oleksandr Polishchuk <aleksice1p@gmail.com>
2025-03-17 20:25:16 +0200

    add task 4

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,18 @@
-function getShippingMessage(country, price, deliveryFee) {
-  // Обчислення загальної вартості замовлення з доставкою
-  const totalPrice = price + deliveryFee;
+function formatMessage(message, maxLength) {
+  // Перевіряє, чи довжина повідомлення не перевищує maxLength
 
-  // Формування та повернення повідомлення
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+  if (message.Length <= maxLength) {
+    return message;
+  } else {
+    return message.slice(0, maxLength) + "...";
+  }
 }
 
-// Перевірка коректності функції
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+// Перевірка функції
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,26 @@
-function getElementWidth(content, padding, border) {
-  // Перетворення значень з рядка у число
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding) * 2;
-  const borderWidth = parseFloat(border) * 2;
+function checkForSpam(message) {
 
-  // Обчислення загальної ширини елемента (border-box)
-  return contentWidth + paddingWidth + borderWidth;
+    // Перетворює текст у нижній регістр, щоб враховувати всі варіанти написання
+    
+  const normalizedMessage = message.toLowerCase();
+
+  // Перевіряємо, чи містить текст слова "spam" або "sale"
+  if (
+    normalizedMessage.includes("spam") ||
+    normalizedMessage.includes("sale")
+  ) {
+    return true; 
+  }
+
+  return false;
 }
 
-// Перевірка коректності функції
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// Перевірка функції:
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
